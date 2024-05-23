e32d90bd84ef676726ca0506fce05f0bc0ec7eec
jenish0307 <131126665+jenish0307@users.noreply.github.com>
2024-05-23 09:12:08 +0530

    Add files via upload

---
+++ NODEJS-1/server.js
@@ -0,0 +1,119 @@
+let http = require("http");
+
+
+
+// http.createServer((req , res)=>{
+//     res.end("hello world")
+// }).listen(3001, ()=> {
+//     console.log("server started 3001");
+// });
+
+// http.createServer((req , res)=>{
+//     res.end("hellow world")
+// }).listen(3002, ()=> {
+//     console.log("server started 3002");
+// });
+
+
+// http.createServer((req , res)=>{
+//     res.end("jenish")
+// }).listen(3003, ()=> {
+//     console.log("server started 3003");
+// });
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end("jenish s. vavadiya")
+// }).listen(3004, ()=> {
+//     console.log("server started 3004");
+// });
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3005")
+// }).listen(3005, ()=> {
+//     console.log("server started 3005");
+// });
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3006")
+// }).listen(3006, ()=> {
+//     console.log("server started 3006");
+// });
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3007")
+// }).listen(3007, ()=> {
+//     console.log("server started 3007");
+// });
+
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3008")
+// }).listen(3008, ()=> {
+//     console.log("server started 3008");
+// });
+
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3009")
+// }).listen(3009, ()=> {
+//     console.log("server started 3009");
+// });
+
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3010")
+// }).listen(3010, ()=> {
+//     console.log("server started 3010");
+// });
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3011")
+// }).listen(3011, ()=> {
+//     console.log("server started 3011");
+// });
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3012")
+// }).listen(3012, ()=> {
+//     console.log("server started 3012");
+// });
+
+
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3013")
+// }).listen(3013, ()=> {
+//     console.log("server started 3013");
+// });
+
+// http.createServer((req , res)=>{
+//     console.log(req.headers);
+//     res.end(" this server started 3014")
+// }).listen(3014, ()=> {
+//     console.log("server started 3014");
+// });
+
+
+http.createServer((req , res)=>{
+    console.log(req.headers);
+    res.end(" this server started 3015")
+}).listen(3015, ()=> {
+    console.log("server started 3015");
+});
